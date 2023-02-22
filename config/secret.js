65ec5efa3707fd052236a8edd4500a4da36e7593
Leenayoung <80760544+lnalice@users.noreply.github.com>
2023-02-22 20:51:13 +0900

    Delete secret.js

--- config/secret.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secretKey: "eyJuYW1lIjoiZmFybVVzIiwidGl0bGUiOiJmYXJtVXMifQ",
-    options : {
-        algorithm : "HS256",
-        expiresIn : "1d",
-        issuer : "farmUs"
-    }
-}
