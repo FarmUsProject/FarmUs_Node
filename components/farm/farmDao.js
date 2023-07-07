a87980229ae62df50c8987280c818ad95d23f7b8
Junho Choi <tig9659@ajou.ac.kr>
2023-07-07 21:42:22 +0900

    농장 검색 시 사진 반환 방식 수정

--- components/farm/farmDao.js
+++ components/farm/farmDao.js
@@ -94,16 +94,23 @@ exports.selectFarmbyFarmInfo = async(connection, sameFarmInfo) =>{
 
 exports.searchFarm = async(connection, keyword) => {
     const searchFarmQuery = `
-    SELECT FarmID,
-			Name,
-			Price,
-			SquaredMeters,
-			LocationBig,
-			LocationMid,
-			LocationSmall,
-			Likes
-    FROM Farm
-    WHERE Name LIKE ? OR LocationBig LIKE ? OR LocationMid LIKE ? OR LocationSmall LIKE ?;`
+    SELECT f.FarmID,
+    f.Name,
+    f.Price,
+    f.SquaredMeters,
+    f.LocationBig,
+    f.LocationMid,
+    f.LocationSmall,
+    f.Likes,
+    fp.Picture_url
+  FROM Farm f
+  LEFT JOIN (
+    SELECT FarmID, Picture_url
+    FROM FarmPictures
+    GROUP BY FarmID
+  ) fp ON f.FarmID = fp.FarmID
+  WHERE f.Name LIKE ? OR f.LocationBig LIKE ? OR f.LocationMid LIKE ? OR f.LocationSmall LIKE ?;`;
+
     console.log(keyword);
     const [farmRow] = await connection.query(searchFarmQuery,[keyword,keyword,keyword,keyword])
     console.log(farmRow);
